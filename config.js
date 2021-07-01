35628da6313b7d54d73baebde46684fd2cd7c862
trungthanhnguyen <86781899+trungthanhnguyen@users.noreply.github.com>
2021-07-01 19:44:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 const CONFIG = {
     introTitle: 'Hồnh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao em không ra ngoài đường`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Phải chăng pạn không mún đi zới mình ',
     btnYes: 'Vẫn cứ là thích đi <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại đi với anh <3',
