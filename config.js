fab022e39a677c7a5543e9d3ac4d22031163dfc8
trungthanhnguyen <86781899+trungthanhnguyen@users.noreply.github.com>
2021-07-01 19:36:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hồnh à!!',
+    introTitle: 'Hồnh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao mày không ra đường`,
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng pạn không muốn đi với mình 😙',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích đi <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại đi với anh <3',
-    btnReply: 'Gửi cho anh Thành',
-    reply: 'ĐI thì đi mà không đi thì đi <33333333',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Đi thì đi mà không đi thì đi <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/thanhot131
+    messLink: 'https://m.m/thanhot131' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
